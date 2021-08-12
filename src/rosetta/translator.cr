4f064bfea446383ab63b62c14ec2bd008998d364
Wout <hi@wout.codes>
2021-08-12 14:59:57 +0100

    refactor: remove user_lang method from translator

--- src/rosetta/translator.cr
+++ src/rosetta/translator.cr
@@ -13,13 +13,6 @@ module Rosetta
     #   I18n.translate(inferred_key_prefix(key), count: count)
     # end
 
-    # In places where current_user / user isn't available be sure to override
-    # this method with:
-    # `def user_lang; I18n.locale; end`
-    def user_lang : String
-      current_user.try(&.lang) || I18n.locale
-    end
-
     # In places where a different key prefix is desired define a period-delimited
     # string to be used instead of a prefix based on the current class name.
     def t_prefix : String?
