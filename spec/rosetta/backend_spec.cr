f05a9050c43ee0255c59575e1b17d3bc1acf58ff
Wout <hi@wout.codes>
2021-08-14 11:32:11 +0100

    refactor: rename look_up macro on backend to find

--- spec/rosetta/backend_spec.cr
+++ spec/rosetta/backend_spec.cr
@@ -1,22 +1,22 @@
 require "../spec_helper"
 
 describe Rosetta::Backend do
-  describe ".look_up" do
+  describe ".find" do
     it "finds an existing key" do
-      Rosetta::Backend.look_up("title")
+      Rosetta::Backend.find("title")
         .should eq({"en" => "Title", "nl" => "Titel"})
-      Rosetta::Backend.look_up("user.first_name")
+      Rosetta::Backend.find("user.first_name")
         .should eq({"en" => "First name", "nl" => "Voornaam"})
     end
 
     # NOTE: uncomment these to see the compilation errors
     # it "shows a compile error for a missing locale key" do
-    #   Rosetta::Backend.look_up("pt", "user.secret")
+    #   Rosetta::Backend.find("pt", "user.secret")
     #     .should eq("Ssssst")
     # end
     #
     # it "shows a compile error for a missing translation" do
-    #   Rosetta::Backend.look_up("en", "user.secret")
+    #   Rosetta::Backend.find("en", "user.secret")
     #     .should eq("Ssssst")
     # end
   end
