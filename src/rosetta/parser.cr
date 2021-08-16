3656e9f3b8c3a611a6ec57f4cd39aecf6e8ba4ec
Wout <hi@wout.codes>
2021-08-16 13:47:24 +0100

    feat: make the parser check the validity of the given translations

--- src/rosetta/parser.cr
+++ src/rosetta/parser.cr
@@ -8,22 +8,37 @@ module Rosetta
     getter path : String
     getter default_locale : String
     getter available_locales : Array(String)
+    getter alternative_locales : Array(String)
     getter translations = TranslationsHash.new
+    getter error : String? = nil
 
     def initialize(
       @path : String,
       @default_locale : String,
       @available_locales : Array(String)
     )
+      @alternative_locales = available_locales - [default_locale]
     end
 
     # Returns a flat list of key/translation pairs as a string.
-    def parse! : String?
+    def parse! : String
       load!
+
+      return error.to_s unless valid?
 
       return "{} of String => Hash(String, String)" if translations.empty?
 
       "#{flip(translations)}"
+    end
+
+    # Tests validity of all locale key sets.
+    def valid? : Bool
+      check_available_locales_present? &&
+        check_key_set_complete? &&
+        check_key_set_overflowing? &&
+        check_interpolation_keys_matching?
+
+      error.nil?
     end
 
     # Loads and parses JSON files, then YAML files, adds them to the list of
@@ -46,13 +61,80 @@ module Rosetta
       end
     end
 
+    # Checks if available locales are present.
+    private def check_available_locales_present? : Bool
+      return true if (diff = available_locales - translations.keys).empty?
+
+      @error = <<-ERROR
+      Expected to find translations for:
+      #{pretty_list_for_error(available_locales)}
+
+      But missing all translations for:
+
+      #{pretty_list_for_error(diff)}
+
+      ERROR
+
+      false
+    end
+
+    # Checks if all key sets have the same keys as the main locale.
+    private def check_key_set_complete? : Bool
+      alternative_locales.each do |locale|
+        diff = ruling_key_set - translations[locale].keys
+
+        unless diff.empty?
+          @error = <<-ERROR
+          Missing keys for locale "#{locale}":
+          #{pretty_list_for_error(diff)}
+
+          ERROR
+
+          return false
+        end
+      end
+
+      true
+    end
+
+    # Checks if other keys sets don't have more keys than the main locale.
+    private def check_key_set_overflowing? : Bool
+      alternative_locales.each do |locale|
+        diff = translations[locale].keys - ruling_key_set
+
+        unless diff.empty?
+          @error = <<-ERROR
+          The "#{locale}" locale has unused keys:
+          #{pretty_list_for_error(diff)}
+
+          ERROR
+
+          return false
+        end
+      end
+
+      true
+    end
+
+    # Checks if interpolation keys are maching in all available locales.
+    private def check_interpolation_keys_matching? : Bool
+      true
+    end
+
+    # Returns the key set of the default locale.
+    private def ruling_key_set
+      translations[default_locale].keys
+    end
+
+    # Generate a visual list for errors from an array of strings.
+    private def pretty_list_for_error(list : Array(String))
+      list.map { |key| "  ‣ #{key}\n" }.join
+    end
+
     # Flips translations from top-level locales to top-level keys.
     private def flip(translations)
-      locales = translations.keys
-      ruling_key_set = translations[default_locale].keys
-
       ruling_key_set.each_with_object(TranslationsHash.new) do |k, h|
-        h[k] = locales.each_with_object({} of String => String) do |l, t|
+        h[k] = available_locales.each_with_object({} of String => String) do |l, t|
           t[l] = translations[l][k]
         end
       end
