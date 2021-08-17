309d769804f13f6c2dd62ffbde64a3a54fcee123
Wout <hi@wout.codes>
2021-08-17 08:14:41 +0100

    test: add specs for parser errors

--- spec/rosetta/parser_spec.cr
+++ spec/rosetta/parser_spec.cr
@@ -27,6 +27,51 @@ describe Rosetta::Parser do
       json["user.first_name"]["en"].should eq("First name")
       json["user.gender.non_binary"]["nl"].should eq("Niet-binair")
     end
+
+    it "returns an error when a complete locale is missing" do
+      output = make_parser(available_locales: %w[en fr nl]).parse!
+
+      output.should eq <<-ERROR
+      Expected to find translations for:
+        ‣ en
+        ‣ fr
+        ‣ nl
+
+
+      But missing all translations for:
+
+        ‣ fr
+
+
+      ERROR
+    end
+
+    it "returns an error when keys are missing" do
+      output = make_parser(available_locales: %w[en en-missing]).parse!
+
+      output.should eq <<-ERROR
+      Missing keys for locale "en-missing":
+        ‣ user.first_name
+        ‣ user.last_name
+        ‣ user.gender.male
+        ‣ user.gender.female
+        ‣ user.gender.non_binary
+
+
+      ERROR
+    end
+
+    it "returns an error when there are overflowing keys" do
+      output = make_parser(available_locales: %w[en en-overflow]).parse!
+
+      output.should eq <<-ERROR
+      The "en-overflow" locale has unused keys:
+        ‣ i_am_overflowing
+        ‣ user.email
+
+
+      ERROR
+    end
   end
 end
 
@@ -36,7 +81,7 @@ def make_parser(
   available_locales = %w[en nl]
 )
   Rosetta::Parser.new(
-    "spec/fixtures/locales",
+    "spec/fixtures/rosetta",
     default_locale,
     available_locales
   )
