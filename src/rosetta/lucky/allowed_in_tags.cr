1fe25bfd4cfbc55aa245a7ee1d4caf3ca9829b50
Wout <hi@wout.codes>
2021-09-10 13:39:35 +0200

    feat: add integration macro for Lucky

--- src/rosetta/lucky/allowed_in_tags.cr
+++ src/rosetta/lucky/allowed_in_tags.cr
@@ -1,4 +1,4 @@
 # Objects including this module can safely be used in Lucky as a value for html
 # tags, without the need to explicitly convert them to a string.
-module Lucky::AllowedInTags
+module ::Lucky::AllowedInTags
 end
---
+++ src/rosetta/lucky/integration.cr
@@ -0,0 +1,25 @@
+module Rosetta
+  module Lucky
+    # Includes `Rosetta::Translatable` where it may be required.
+    macro integrate
+      {% targets = {
+           "Avram::Model",
+           "Avram::Operation",
+           "Lucky::Action",
+           "Lucky::BaseComponent",
+         } %}
+
+      {% for target in targets %}
+        abstract class ::{{target.id}}
+          include Rosetta::Translatable
+        end
+      {% end %}
+
+      module ::Lucky::HTMLPage
+        macro included
+          include Rosetta::Translatable
+        end
+      end
+    end
+  end
+end
