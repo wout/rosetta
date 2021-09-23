d5bd9301de0065802e21adaf559ff0331cfb4d4c
Wout <hi@wout.codes>
2021-09-23 16:06:17 +0200

    refactor: move parser checks to a separate module

---
+++ src/rosetta/parser/checks.cr
@@ -0,0 +1,88 @@
+module Rosetta
+  class Parser
+    module Checks
+      # Checks if available locales are present.
+      private def check_available_locales_present? : Bool
+        return true if (diff = available_locales - translations.keys).empty?
+
+        @error = <<-ERROR
+        Expected to find translations for:
+        #{pretty_list_for_error(available_locales)}
+
+        But missing all translations for:
+
+        #{pretty_list_for_error(diff)}
+
+        ERROR
+
+        false
+      end
+
+      # Checks if all key sets have the same keys as the main locale.
+      private def check_key_set_complete? : Bool
+        alternative_locales.each do |locale|
+          diff = ruling_key_set - translations[locale].keys
+
+          unless diff.empty?
+            @error = <<-ERROR
+            Missing keys for locale "#{locale}":
+            #{pretty_list_for_error(diff)}
+
+            ERROR
+
+            return false
+          end
+        end
+
+        true
+      end
+
+      # Checks if other keys sets don't have more keys than the main locale.
+      private def check_key_set_overflowing? : Bool
+        alternative_locales.each do |locale|
+          diff = translations[locale].keys - ruling_key_set
+
+          unless diff.empty?
+            @error = <<-ERROR
+            The "#{locale}" locale has unused keys:
+            #{pretty_list_for_error(diff)}
+
+            ERROR
+
+            return false
+          end
+        end
+
+        true
+      end
+
+      # Checks if interpolation keys are maching in all available locales.
+      private def check_interpolation_keys_matching? : Bool
+        errors = ruling_key_set.each_with_object([] of String) do |k, e|
+          ruling_translation = processed_translations[k][default_locale]
+          i12n_keys = ruling_translation.to_s.scan(/%\{[^\}]+\}/).map { |m| m[0] }
+
+          next if i12n_keys.empty?
+
+          alternative_locales.each do |l|
+            i12n_keys.each do |key|
+              next if processed_translations[k][l].to_s.match(%r{#{key}})
+
+              e << %(#{l}: #{k} should contain "#{key}")
+            end
+          end
+        end
+
+        unless errors.empty?
+          @error = <<-ERROR
+          Some translations have mismatching interpolation keys:
+          #{pretty_list_for_error(errors)}
+
+          ERROR
+        end
+
+        true
+      end
+    end
+  end
+end
