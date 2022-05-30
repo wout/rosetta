38faf59c689c0ae116f2047442ecdf0977fdfb7c
Wout <hi@wout.codes>
2022-05-30 16:05:50 +0200

    feat: add better integration for Lucky

--- src/rosetta/lucky/integration.cr
+++ src/rosetta/lucky/integration.cr
@@ -26,3 +26,27 @@ module Lucky::HTMLPage
     include Rosetta::Translatable
   end
 end
+
+class Lucky::FlashStore
+  {% for shortcut in [:failure, :info, :success] %}
+    def {{ shortcut.id }}=(message : Rosetta::Translation)
+      set(:{{ shortcut.id }}, message.t)
+    end
+  {% end %}
+
+  def set(key : Key, value : Rosetta::Translation) : String
+    set(key, value.t)
+  end
+end
+
+module Lucky::SpecialtyTags
+  def raw(string : Rosetta::Translation) : Nil
+    view << string.t
+  end
+end
+
+module Lucky::FormHelpers
+  def submit(text : Rosetta::Translation, **html_options) : Nil
+    submit(text.t, **html_options)
+  end
+end
