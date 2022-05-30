8758abd99c00332418c08d7f80e3bdec0e0f50a8
Wout <hi@wout.codes>
2022-05-30 16:55:44 +0200

    feat: add avram backend integrations

--- src/rosetta/lucky/integration.cr
+++ src/rosetta/lucky/integration.cr
@@ -50,3 +50,38 @@ module Lucky::FormHelpers
     submit(text.t, **html_options)
   end
 end
+
+struct Rosetta::AvramBackend < Avram::I18nBackend
+  def get(key : String | Symbol) : String
+    {% begin %}
+      case key
+      {% for val in %w[
+                      exact_size_of
+                      max_size_of
+                      min_size_of
+                      numeric_max
+                      numeric_min
+                    ] %}
+      when :validate_{{val.id}}
+        Rosetta.find("avram.validate_{{val.id}}").t(size: "%d")
+      {% end %}
+      {% for val in %w[
+                      acceptance_of
+                      at_most_one_filled
+                      confirmation_of
+                      exactly_one_filled
+                      format_of
+                      inclusion_of
+                      numeric_nil
+                      required
+                      uniqueness_of
+                    ] %}
+      when :validate_{{val.id}}
+        Rosetta.find("avram.validate_{{val.id}}").t
+      {% end %}
+      else
+        raise "Avram translation missing for '#{key}'"
+      end
+    {% end %}
+  end
+end
